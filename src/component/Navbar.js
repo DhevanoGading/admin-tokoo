d24b07a9d9dd3048800df13ada46584279b856f1
Dhevano Gading <dhevanogading@gmail.com>
2022-05-12 22:33:54 +0700

    commit 2

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 class Navbar extends React.Component {
     Logout = () => {
         localStorage.removeItem("token")
-        localStorage.removeItem("petugas")
+        localStorage.removeItem('pegawai')
         window.location = "/login"
     }
     render() {
@@ -23,7 +23,7 @@ class Navbar extends React.Component {
                         <li className="nav-item">
                             <Link to="/" className="nav-link">
                                 Home
-                                
+
                             </Link>
                         </li>
                         <li className="nav-item">
